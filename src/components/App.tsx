006aaebd828bb6d0370f1863d734f3136451a25d
feninTolia <fenintolja@gmail.com>
2023-01-17 12:12:41 +0200

    DONE

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+import { ITodo } from '../types/data';
+import TodoList from './TodoList';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') addTodo();
+  };
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+      setValue('');
+    }
+  };
+
+  const removeTodo = (id: number): void => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id: number): void => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      })
+    );
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={handleChange}
+          ref={inputRef}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+    </div>
+  );
 };
 
 export default App;
